d214b3e0b19f07ac43d008cd61fbeaadf1b2a9a7
siddheshpagar <pagarsiddhesh2000@gmail.com>
2025-04-04 19:16:41 +0530

    Backend is running smoothly 🚀 | All features working as expected ✅

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -64,7 +64,6 @@ export const loginAdmin = async (request, response) => {
                     httpOnly: true,  // Makes it not accessible to client side script
                     secure: process.env.NODE_ENV === 'production', // Ensure secure cookies in production
                     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
-                    domain: '.onrender.com',
                     // domain: process.env.NODE_ENV === 'production' ? "blogify-frontend-4sur.onrender.com" : 'localhost',
                     path: "/",
                 });
